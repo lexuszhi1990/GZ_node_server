995efeb46780101022902539501692d0f7327d83
David <david@beansmile.com>
2014-04-26 02:26:22 +0800

    refactor the appjs

--- app.js
+++ app.js
@@ -1,21 +1,24 @@
-var express = require('express')
-  , routes = require('./routes')
-  , http = require('http');
+var express = require('express');
+var routes = require('./routes');
+var user = require('./routes/user');
+var http = require('http');
+var path = require('path');
 
 var app = express();
 var server = app.listen(8888);
 var io = require('socket.io').listen(server);
 
 app.configure(function(){
-  app.set('views', __dirname + '/views');
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', path.join(__dirname, 'views'));
   app.set('view engine', 'jade');
   app.use(express.favicon());
   app.use(express.logger('dev'));
-  app.use(express.static(__dirname + '/public'));
   app.use(express.json());
   app.use(express.urlencoded());
   app.use(express.methodOverride());
   app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
 });
 
 app.configure('development', function(){
@@ -24,9 +27,20 @@ app.configure('development', function(){
 
 app.get('/', routes.index);
 
-console.log("Express server listening on port" + 8888);
+console.log('Express server listening on port ' + app.get('port'));
 
 io.on('connection', function (socket) {
   var addedUser = false;
-  console.log("log here");
-}
+  console.log("One connection comess");
+
+  socket.on('left', function (data) {
+    // we tell the client to execute 'new message'
+    // socket.broadcast.emit('new message', {
+    //   username: socket.username,
+    //   message: data
+    // });
+
+    // move left
+    console.log("move left");
+  });
+});
